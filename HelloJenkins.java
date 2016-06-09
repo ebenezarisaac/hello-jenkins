98d6ddf18be13665a84fc671b79dc131bdd95aa4
ebenezarisaac <isaacspring14@gmail.com>
2016-06-09 16:56:33 -0500

    Update HelloJenkins.java

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -4,5 +4,7 @@ public class HelloJenkins {
 		System.out.println("Welcome to your first Jenkins Job");
 		System.out.println("Files has been updated here in the child branch");
 		System.out.println("Let's test if the continous integration is working here");
+		System.out.println("---------------------------------------------------");
+		System.out.println("Github is polling Jenkins");
 	}
 }
